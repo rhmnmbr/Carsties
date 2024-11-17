2cb5b7ee1ae9c7269dce0832278385e4862407ac
rhmnmbr <rhmnmbr83@gmail.com>
2024-11-17 09:55:08 +0700

    End of section 9

---
+++ frontend/web-app/app/api/auth/signin/page.tsx
@@ -0,0 +1,14 @@
+import EmptyFilter from '@/app/components/EmptyFilter'
+import React from 'react'
+
+export default async function SignIn({ searchParams }: { searchParams: { callbackUrl: string } }) {
+  const { callbackUrl } = await searchParams
+  return (
+    <EmptyFilter
+      title='You need to be logged in to do that'
+      subtitle='Please click below to login'
+      showLogin
+      callbackUrl={callbackUrl}
+    />
+  )
+}
